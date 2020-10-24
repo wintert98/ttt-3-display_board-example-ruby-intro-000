f3627c6f39e33c91d7b7fda4c76f25e84e8111d5
Thomas Winter <wintert98@gmail.com>
2020-10-24 18:58:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "  |  |  "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
